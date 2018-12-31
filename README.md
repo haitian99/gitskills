ba4385953a83211a8a3560fabed97a1959bf522a
liurujiu <liurujiu@gmail.com>
2018-12-31 23:18:45 +0800

    ver1 本地第1次修改。添加 main.cpp

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gitskills
 a git repositoy for learning how to use git, step2.
+
+1 add file  "main.cpp"
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+/// a hello world 
+
+#include <iostream>
+
+int main()
+{
+	ccout<<"Hello, world"<<endl;
+	return 0;
+}
